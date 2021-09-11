d079273b0972c2605a256a4c5ce7e138b4ac9fce
Bowhza <bogtank2003@gmail.com>
2021-09-10 18:07:55 -0600

    Cleaned up the code to make it more readable.

--- game.js
+++ game.js
@@ -1,17 +1,13 @@
 const choices = ["rock", "paper", "scissors"]
-
-class score {
-    constructor(score){
-        this.score = +score;
-    }
-}
-
-let playerScore = new score(0);
-let computerScore = new score(0);
 const scoreLimit = 5;
 const gameOn = true;
 
+let playerScore = 0;
+let computerScore = 0;
+
+
 function game(){
+
     function computerPlay(cpuChoice){
         cpuChoice = choices[Math.floor(Math.random() * choices.length)];
         return cpuChoice;
@@ -33,30 +29,30 @@ function game(){
     function playRound(playerSelection, computerSelection){
     
         console.log(`You Chose: ${playerSelection.toUpperCase()} | The Computer Chose: ${computerSelection.toUpperCase()}`);
-    
+        
         if (playerSelection === "rock" && computerSelection === "scissors"){
             console.log("You Win! Rock beats Scissors!");
-            playerScore.score += 1;
-            console.log(`Your Score: ${playerScore.score} | Computer Score: ${computerScore.score}`);
+            playerScore += 1;
+            console.log(`Your Score: ${playerScore} | Computer Score: ${computerScore}`);
         }
         else if (playerSelection === "paper" && computerSelection === "rock"){
             console.log("You Win! Paper beats Rock!");
-            playerScore.score += 1;
-            console.log(`Your Score: ${playerScore.score} | Computer Score: ${computerScore.score}`);
+            playerScore += 1;
+            console.log(`Your Score: ${playerScore} | Computer Score: ${computerScore}`);
         }
         else if (playerSelection === "scissors" && computerSelection === "paper"){
             console.log("You Win! Scissors beats Paper!");
-            playerScore.score += 1;
-            console.log(`Your Score: ${playerScore.score} | Computer Score: ${computerScore.score}`);
+            playerScore += 1;
+            console.log(`Your Score: ${playerScore} | Computer Score: ${computerScore}`);
         }
         else if (playerSelection === computerSelection){
             console.log("You Tied!");
-            console.log(`Your Score: ${playerScore.score} | Computer Score: ${computerScore.score}`);
+            console.log(`Your Score: ${playerScore} | Computer Score: ${computerScore}`);
         }
         else {
             console.log(`You Lose! ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1)}.`);
-            computerScore.score += 1;
-            console.log(`Your Score: ${playerScore.score} | Computer Score: ${computerScore.score}`);
+            computerScore += 1;
+            console.log(`Your Score: ${playerScore} | Computer Score: ${computerScore}`);
         }
     }
     const computerSelection = computerPlay();
@@ -65,14 +61,13 @@ function game(){
 }
 
 while (gameOn){ 
-    if (playerScore.score === scoreLimit){
+    if (playerScore === scoreLimit){
         console.log("You Win!");
         break
     }
-    else if (computerScore.score === scoreLimit){
+    else if (computerScore === scoreLimit){
         console.log("Computer Wins!");
         break
     }
-
     game();
 }
